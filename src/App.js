dca76ad45da9f4669910df0fc17111d622ee7f22
Dustin DAvignon <ddavignon@csumb.edu>
2017-06-29 04:54:35 +0000

    feat: Navigation with react router dom

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import SearchPage from './components/SearchPage';
 import BooksList from './components/BooksList';
 import * as BooksAPI from './BooksAPI';
 import './App.css';
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true
-  }
-
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchPage />
-        ) : (
-          <BooksList />
-        )}
+        <Route path="/search" component={SearchPage}/>
+        <Route path="/books" component={BooksList} />
       </div>
     )
   }
