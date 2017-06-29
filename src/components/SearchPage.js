89f1a5ea2a196db67f68f74cb50378fe870fc210
Dustin DAvignon <ddavignon@csumb.edu>
2017-06-29 06:07:47 +0000

    feat: Connect Book List Detail to search page

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,29 +1,46 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import BooksListDetail from './BooksListDetail';
+import * as BooksAPI from '../BooksAPI';
 
 
 class SearchPage extends Component {
+  state = {
+    books: []
+  }
+  
+  componentDidMount() {
+    BooksAPI.getAll().then(books => this.setState({ books }));
+  }
     
-    render() {
-        return (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link
-                to='/'
-                className='close-search'
-              >
-                Close
-              </Link>
-              <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author"/>
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
+  renderBooks() {
+    return this.state.books.map((items, index) => {
+      return <BooksListDetail key={index} books={items} />;
+    }); 
+  }
+    
+  render() {
+      return (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <Link
+              to='/'
+              className='close-search'
+            >
+              Close
+            </Link>
+            <div className="search-books-input-wrapper">
+              <input type="text" placeholder="Search by title or author"/>
             </div>
           </div>
-        );
-    }
+          <div className="search-books-results">
+            <ol className="books-grid">
+            {this.renderBooks()}
+            </ol>
+          </div>
+        </div>
+      );
+  }
 }
 
 export default SearchPage;
